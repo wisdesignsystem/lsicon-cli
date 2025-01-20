8a4e1604e4eea3bdb0b5fb60703ae69487a61529
FaureWu <faure@wis.design>
2025-01-20 22:38:50 +0800

    chore: create the basic framework

---
+++ commitlint.config.ts
@@ -0,0 +1,21 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // new feature
+        "fix", // fix bug
+        "hotfix", // hotfix bug
+        "doc", // doc change
+        "style", // style change
+        "test", // test code change
+        "chore", // build and deploy
+        "release", // publish version
+        "prerelease", // pre-publish version
+        "changeset-release", // auto changeset publish version
+      ],
+    ],
+  },
+};
---
+++ src/build.ts
@@ -0,0 +1,9 @@
+interface BuildOption {
+  file: string;
+  page: string;
+  token: string;
+}
+
+function build(option: BuildOption) {}
+
+export default build;
